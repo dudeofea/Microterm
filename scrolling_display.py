661283eb9691665b1f40a97a1cab18a48cffdb0c
Denis Lachance <pernicious.pickle@gmail.com>
2020-07-14 08:52:33 -0700

    Started scrolling display, added smaller font, moved A0/RST/CS to TPIC6B595

---
+++ scrolling_display.py
@@ -0,0 +1,59 @@
+import font_utils
+import st7565
+
+class ScrollingDisplay():
+    def __init__(self, font):
+        self.display = st7565.ST7565()
+        self.display.set_contrast(0x20)
+        self.cursor = [0, 0]
+        self.buf = ""
+        self.buf_offset = 0
+        self.disp_offset = 0
+        self.font = font
+        self.display_width = font_utils.get_max_char_width(font)
+
+    def add_string(self, input_str):
+        self.buf += input_str
+        self.display.string(input_str, self.cursor, self.font)
+        self.display.show()
+
+    def delete_char(self):
+        self.buf = self.buf[:-1]
+        # back up cursor
+        self.cursor[0] -= 1
+        if self.cursor[0] < 0 and self.cursor[1] > 0:
+            self.cursor[1] -= 1
+            self.cursor[0] = self.display_width - 1
+        self.display.string(chr(127), [self.cursor[0], self.cursor[1]], self.font)
+        self.display.show()
+
+    def redraw(self):
+        # split lines longer than screen width
+        lines = []
+        for l in self.buf.split('\n'):
+            while len(l) > 0:
+                lines.append(l[:self.display_width])
+                l = l[self.display_width:]
+
+        # redraw all lines padded/
+        for i in range(-self.disp_offset, len(lines)):
+            # pad lines with blanks
+            pad = self.display_width - len(lines[i])
+            print(lines, pad)
+            self.display.string(lines[i] + pad * " ",
+                                [0, i + self.disp_offset],
+                                self.font)
+        self.display.show()
+
+    def scroll_down(self):
+        self.disp_offset -= 1
+        self.buf_offset = self.buf.find('\n', self.buf_offset)
+        # blank out current line, then redraw
+        self.display.string(self.display_width * " ",
+                            [0, self.cursor[1]],
+                            self.font)
+        self.cursor[1] -= 1
+        self.redraw()
+
+    def scroll_up(self):
+        pass
--- st7565.py
+++ st7565.py
@@ -38,18 +38,26 @@ DISPLAY_POWER_MODE = 7
 class ST7565:
     """ST7565 Display controller driver"""
     def __init__(self):
-        self.spi = busio.SPI(board.SCK, MOSI=board.MOSI)
-        self.rst = digitalio.DigitalInOut(board.A0)
+        #self.mosi = digitalio.DigitalInOut(board.MOSI)
+        #self.sck = digitalio.DigitalInOut(board.SCK)
         self.a0 = digitalio.DigitalInOut(board.A2)
-        self.cs = digitalio.DigitalInOut(board.A1)
+        self.rst = digitalio.DigitalInOut(board.A1)
+        self.cs = digitalio.DigitalInOut(board.A0)
         self.width = DISPLAY_W
         self.height = DISPLAY_H
         self.buffer = bytearray(1024)
+
+        #self.cs.direction = digitalio.Direction.OUTPUT
+        #self.shift_write(0xFF)
+        #self.shift_write(0x00)
+        #self.shift_write(0xFF)
         self.display_init()
 
     def display_init(self):
-        self.rst.direction = digitalio.Direction.OUTPUT
+        #self.mosi.direction = digitalio.Direction.OUTPUT
+        #self.sck.direction = digitalio.Direction.OUTPUT
         self.a0.direction = digitalio.Direction.OUTPUT
+        self.rst.direction = digitalio.Direction.OUTPUT
         self.cs.direction = digitalio.Direction.OUTPUT
         self.reset()
         time.sleep(0.001)
@@ -66,27 +74,49 @@ class ST7565:
         self.show()
         self.write_cmd(CMD_DISPLAY_ON)
 
-    def write_cmd(self, cmd):
-        while not self.spi.try_lock():
+    def spi_write(self, byte_buf):
+        spi = busio.SPI(board.SCK, MOSI=board.MOSI)
+        while not spi.try_lock():
             pass
-        try:
-            self.a0.value = False
+        self.cs.value = False
+        spi.write(byte_buf)
+        self.cs.value = True
+        spi.unlock()
+        spi.deinit()
+        #for b in byte_buf:
+        #    for i in range(7, -1, -1):
+        #        self.sck.value = False
+        #        self.mosi.value = (b & 1 << i) != 0
+        #        self.sck.value = True
+
+    def shift_write(self, byte_val):
+        mosi = digitalio.DigitalInOut(board.MOSI)
+        rck = digitalio.DigitalInOut(board.SCK)
+        mosi.direction = digitalio.Direction.OUTPUT
+        rck.direction = digitalio.Direction.OUTPUT
+
+        rck.value = False
+        for i in range(7, -1, -1):
+            # SRCK on shift register is shared with CS
             self.cs.value = False
-            self.spi.write(bytes([cmd]))
+            mosi.value = (byte_val & 1 << i) != 0
             self.cs.value = True
-        finally:
-            self.spi.unlock()
+        time.sleep(0.001)
+        rck.value = True
+        time.sleep(0.001)
+        rck.value = False
+        mosi.deinit()
+        rck.deinit()
+
+    def write_cmd(self, cmd):
+        #self.a0.value = False
+        self.shift_write(0x01)
+        self.spi_write(bytes([cmd]))
 
     def write_data(self, buf):
-        while not self.spi.try_lock():
-            pass
-        try:
-            self.a0.value = True
-            self.cs.value = False
-            self.spi.write(buf)
-            self.cs.value = True
-        finally:
-            self.spi.unlock()
+        #self.a0.value = True
+        self.shift_write(0x00)
+        self.spi_write(buf)
 
     def set_contrast(self, value):
         if 0x1 <= value <= 0x3f:
@@ -97,7 +127,9 @@ class ST7565:
 
     def reset(self):
         self.rst.value = False
+        self.shift_write(0x00)
         time.sleep(0.001)
+        self.shift_write(0x02)
         self.rst.value = True
 
     def show(self):
@@ -111,10 +143,11 @@ class ST7565:
             self.write_data(self.buffer[i*128:(i+1)*128])
 
     def char(self, x, y, character, width):
-        for i in range(width - 1):
+        for i in range(len(character)):
             self.buffer[(8 - y - 1)*128 + x*width + i] = character[i]
 
     def string(self, input_str, input_pos, font_map):
+        print(input_str, input_pos)
         # TODO: don't assume font's are 5x7 w/o spacing
         f_width = font_map["width"] + 1
         width = (128 - (128 % f_width)) / f_width
